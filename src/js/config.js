8ae92a3f141fab297f022d6093a3cc95d2878204
mubiramshi9494 <mubiramshi9494@gmail.com>
2023-03-27 12:22:30 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b798c395-1921-4c5b-8545-c286a431ef93';
+export const MODAL_CLOSE_SEC = 2.5;
